e585b02bd9ae6795e32ef7d36b91a920277fe028
Piotr Miloszewicz <piotr.miloszewicz@idea-commerce.com>
2022-06-21 19:44:46 +0200

    fix: round down number

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -50,7 +50,7 @@ class Slider {
             this.productWidth = this.lists[0].firstElementChild.offsetWidth
         }
 
-        this.productsPerSingleView = (this.track.offsetWidth - ((this.dataset.configMatrixCols-1) * parseInt(this.dataset.configFrameMarginBetween)))/this.productWidth;
+        this.productsPerSingleView = Math.floor((this.track.offsetWidth - ((this.dataset.configMatrixCols-1) * parseInt(this.dataset.configFrameMarginBetween)))/this.productWidth);
 
         if (this.frame.querySelector('.flowbox__arrows')) {
             const next = this.frame.querySelector('.flowbox__arrow--next')
